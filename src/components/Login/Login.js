b4c08fac846f0f4063625c1222aaae153ecc9c79
Shamim <yeasinshamim7@gmail.com>
2022-04-09 12:47:28 +0600

    log in form init

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,17 +1,20 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 import './Login.css';
 
 const Login = () => {
+    const { user, singInWithGoogle } = useFirebase();
+
     return (
         <div>
             <h3>Please LogIn </h3>
             <div style={{ margin: '20px' }}>
-                <button>Google login</button>
+                <button onClick={singInWithGoogle}>Google login</button>
             </div>
             <form className='login-form'>
-                <input type="email" name="" id="" placeholder='Your Email' />
+                <input type="email" name="" id="email-input" placeholder='Your Email' />
                 <br />
-                <input type="password" name="" id="" placeholder='password' />
+                <input type="password" name="" id="password-input" placeholder='password' />
                 <br />
                 <input type="submit" value="Login" />
             </form>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,9 +1,26 @@
-import { useState } from "react"
+import { useState } from "react";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import app from "../firebase.init";
+
+const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const useFirebase = () => {
     const [user, setUser] = useState({});
 
-    return {};
+    const singInWithGoogle = () => {
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const user = result.user;
+                setUser(user);
+                console.log(user);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    return { user, singInWithGoogle };
 }
 
 export default useFirebase;
