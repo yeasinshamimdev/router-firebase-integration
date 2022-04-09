4f8b8b6b366261a45dd8f8819a4b20faf8a20df0
Shamim <yeasinshamim7@gmail.com>
2022-04-09 17:40:32 +0600

    react firebase hooks using

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Home = () => {
+    const { user } = useFirebase()
     return (
         <div>
             <h1>this is Home</h1>
+            <h4>Current User is: {user ? user.displayName : 'No user found'}</h4>
         </div>
     );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,18 @@
 import React from 'react';
-import useFirebase from '../../hooks/useFirebase';
 import './Login.css';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import app from '../../firebase.init';
+
+const auth = getAuth(app);
 
 const Login = () => {
-    const { user, singInWithGoogle } = useFirebase();
-
+    const [user,] = useAuthState(auth);
     return (
         <div>
             <h3>Please LogIn </h3>
             <div style={{ margin: '20px' }}>
-                <button onClick={singInWithGoogle}>Google login</button>
+                <button>Google login</button>
             </div>
             <form className='login-form'>
                 <input type="email" name="" id="email-input" placeholder='Your Email' />
